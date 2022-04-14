793a6a8b62074650821972236cdbc9014a6bd783
Vladimir Vincan <vladimirvincan@gmail.com>
2022-04-15 01:03:14 +0200

    repostioned small robot

--- mep3_simulation/webots_data/controllers/judge/judge.py
+++ mep3_simulation/webots_data/controllers/judge/judge.py
@@ -39,8 +39,8 @@ def main():
     # Robots
     robot_big = ObjectManipulator(supervisor, 'ROBOT_BIG')
     robot_opponent_big = ObjectManipulator(supervisor, 'ROBOT_OPPONENT_BIG')
-    robot_opponent_small = ObjectManipulator(
-        supervisor, 'ROBOT_OPPONENT_SMALL')
+    robot_opponent_small = ObjectManipulator(supervisor,
+                                             'ROBOT_OPPONENT_SMALL')
 
     # Statuette and replica
     statuette = ObjectManipulator(supervisor, 'STATUETTE')
@@ -57,7 +57,7 @@ def main():
         statuette.set_position(x=-1.267, y=-0.768, z=0.125, theta=0.785)
         replica.set_position(x=-1.2621, y=0.17, z=0.1579, theta=pi)
     else:
-        robot_big.set_position(x=1.21, y=0.17, theta=pi)
+        robot_big.set_position(x=1.201, y=0.463, theta=pi)
         robot_opponent_big.set_position(x=-1.26, y=0.46, theta=0)
         robot_opponent_small.set_position(x=-1.26, y=0.128, theta=0)
 
